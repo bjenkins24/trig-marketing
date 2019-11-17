08399d4ab579b4ea28e97fc60006138c972471ae
Brian Jenkins <bjenkins@yourmusiclessons.com>
2019-11-17 14:06:55 -0800

    Add styled components cause I can

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import styled from "styled-components"
 
-export default () => <div>Hello world friends!</div>
+const Container = styled.div`
+  background: black;
+  color: white;
+`
+
+const Index = () => {
+  return <Container>Hello world friends!</Container>
+}
+
+export default Index
