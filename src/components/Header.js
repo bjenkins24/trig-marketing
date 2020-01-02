8c472d576fc47ce871c1cfdfc0fddf7d7a0a29a7
Brian Jenkins <bjenkins@yourmusiclessons.com>
2020-01-01 17:25:09 -0800

    Header change colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Button } from '@trig-app/core-components/dist/Buttons';
 import { BodyBig } from '@trig-app/core-components/dist/Typography';
@@ -8,33 +8,61 @@ import useSiteMetadata from '../helpers/hooks/useSiteMetadata';
 
 const Container = styled.div`
   display: flex;
+  align-items: center;
   position: fixed;
   width: calc(100% - 6.4rem);
   top: 0;
-  background: ${({ theme }) => theme.p};
-  padding: 3.2rem;
+  background: ${({ theme, isLight }) => (isLight ? theme.bs[200] : theme.p)};
+  transition: all 0.3s;
+  padding: ${({ isLight }) => (isLight ? '1.6rem 3.2rem' : '3.2rem')};
   z-index: 100;
+  box-shadow: ${({ theme, isLight }) => (isLight ? theme.sh : 'none')};
 `;
 
 const Nav = styled(HorizontalGroup)`
   margin-left: auto;
 `;
 
+const NavigationItem = styled(BodyBig)`
+  color: ${({ theme, isLight }) => (isLight ? theme.p : theme.pc)};
+`;
+
 const Header = props => {
   const { siteTitle } = useSiteMetadata();
+  const [isLight, setIsLight] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.pageYOffset > 50) {
+        return setIsLight(true);
+      }
+      return setIsLight(false);
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  });
+
   return (
-    <Container {...props}>
-      <Logo title={`${siteTitle} Logo`} />
+    <Container isLight={isLight} {...props}>
+      <Logo
+        css={`
+          margin-top: 0.4rem;
+        `}
+        type={isLight ? 'light' : 'dark'}
+        title={`${siteTitle} Logo`}
+      />
       <Nav margin={3.2}>
-        <BodyBig color="sc" weight="bold">
+        <NavigationItem isLight={isLight} weight="bold">
           Home
-        </BodyBig>
-        <BodyBig color="sc" weight="bold">
+        </NavigationItem>
+        <NavigationItem isLight={isLight} weight="bold">
           Pricing
-        </BodyBig>
-        <BodyBig color="sc" weight="bold">
+        </NavigationItem>
+        <NavigationItem isLight={isLight} weight="bold">
           Sign in
-        </BodyBig>
+        </NavigationItem>
         <Button>Try Now</Button>
       </Nav>
     </Container>
