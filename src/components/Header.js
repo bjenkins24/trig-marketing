2b4205b09127b72aeffca88549b70443fed51c0a
Brian Jenkins <bjenkins@yourmusiclessons.com>
2020-01-03 16:54:50 -0800

    First pass at clearing the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Button } from '@trig-app/core-components/dist/Buttons';
-import { BodyBig } from '@trig-app/core-components/dist/Typography';
+import { BodyBig, HugeStyles } from '@trig-app/core-components/dist/Typography';
 import { HorizontalGroup } from '@trig-app/core-components/dist/Groups';
+import Icon from '@trig-app/core-components/dist/Icon';
 import Logo from '@trig-app/core-components/dist/Logo';
 import { device } from '@trig-app/constants';
 import useSiteMetadata from '../helpers/hooks/useSiteMetadata';
@@ -29,19 +30,60 @@ const Nav = styled(HorizontalGroup)`
 
 const NavigationItem = styled(BodyBig)`
   color: ${({ theme, isLight }) => (isLight ? theme.p : theme.pc)};
+  display: block;
 `;
 
-const HamburgerNav = styled.div`
+const Hamburger = styled(Icon).attrs({
+  type: 'hamburger',
+})`
   display: none;
-  margin-left: auto;
   @media ${device.xs} {
     display: block;
   }
+  position: fixed;
+  z-index: 102;
+  right: 3.2rem;
+  top: 3.8rem;
+`;
+
+const MobileMenu = styled.div`
+  display: none;
+  padding-top: 4.8rem;
+  @media ${device.xs} {
+    display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
+  }
+  height: calc(100% - 4.8rem);
+  position: fixed;
+  width: 100%;
+  padding: 3.2rem;
+  top: 0;
+  z-index: 101;
+  background: ${({ theme, isLight }) => (isLight ? theme.bs[200] : theme.p)};
+  color: white;
+`;
+
+const MobileMenuContent = styled.div`
+  align-self: center;
+  display: flex;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const MobileMenuNav = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const MobileMenuNavItem = styled.li`
+  ${HugeStyles}
+  color: ${({ theme }) => theme.pc}
 `;
 
 const Header = props => {
   const { siteTitle } = useSiteMetadata();
   const [isLight, setIsLight] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
@@ -57,28 +99,40 @@ const Header = props => {
   });
 
   return (
-    <Container isLight={isLight} {...props}>
-      <Logo
-        css={`
-          margin-top: 0.4rem;
-        `}
-        type={isLight ? 'light' : 'dark'}
-        title={`${siteTitle} Logo`}
-      />
-      <Nav margin={3.2}>
-        <NavigationItem isLight={isLight} weight="bold">
-          Home
-        </NavigationItem>
-        <NavigationItem isLight={isLight} weight="bold">
-          Pricing
-        </NavigationItem>
-        <NavigationItem isLight={isLight} weight="bold">
-          Sign in
-        </NavigationItem>
-        <Button>Try Now</Button>
-      </Nav>
-      <HamburgerNav>My test</HamburgerNav>
-    </Container>
+    <>
+      <Container isLight={isLight} {...props}>
+        <Logo
+          css={`
+            margin-top: 0.4rem;
+          `}
+          type={isLight ? 'light' : 'dark'}
+          title={`${siteTitle} Logo`}
+        />
+        <Nav margin={3.2}>
+          <NavigationItem isLight={isLight} weight="bold">
+            Home
+          </NavigationItem>
+          <NavigationItem isLight={isLight} weight="bold">
+            Pricing
+          </NavigationItem>
+          <NavigationItem isLight={isLight} weight="bold">
+            Sign in
+          </NavigationItem>
+          <Button>Try Now</Button>
+        </Nav>
+      </Container>
+      <Hamburger onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} />
+      <MobileMenu isOpen={isMobileMenuOpen}>
+        <MobileMenuContent>
+          <MobileMenuNav>
+            <MobileMenuNavItem>Home</MobileMenuNavItem>
+            <MobileMenuNavItem>Pricing</MobileMenuNavItem>
+            <MobileMenuNavItem>Sign In</MobileMenuNavItem>
+            <MobileMenuNavItem>Try Now</MobileMenuNavItem>
+          </MobileMenuNav>
+        </MobileMenuContent>
+      </MobileMenu>
+    </>
   );
 };
 
