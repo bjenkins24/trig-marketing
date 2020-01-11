78d20d65875ca252cd551e504a19b23f90ec4af8
Brian Jenkins <bjenkins@yourmusiclessons.com>
2020-01-10 21:03:54 -0800

    Format globalcss

--- global.css.js
+++ global.css.js
@@ -10,57 +10,57 @@ import {
 import 'normalize.css';
 
 export default createGlobalStyle`
-  @font-face {
-      font-family: 'hero new';
-      font-style: normal;
-      font-weight: 400;
-      src: local('Hero New Regular'), url(${HeroNew}) format('woff');
-  }
+    @font-face {
+        font-family: 'hero new';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Hero New Regular'), url(${HeroNew}) format('woff');
+    }
 
-  @font-face {
-      font-family: 'hero new';
-      font-style: italic;
-      font-weight: 400;
-      src: local('Hero New Regular Italic'), url(${HeroNewItalic}) format('woff');
-  }
+    @font-face {
+        font-family: 'hero new';
+        font-style: italic;
+        font-weight: 400;
+        src: local('Hero New Regular Italic'), url(${HeroNewItalic}) format('woff');
+    }
 
-  @font-face {
-      font-family: 'hero new';
-      font-style: normal;
-      font-weight: 500;
-      src: local('Hero New SemiBold'), url(${HeroNewSemiBold}) format('woff');
-  }
+    @font-face {
+        font-family: 'hero new';
+        font-style: normal;
+        font-weight: 500;
+        src: local('Hero New SemiBold'), url(${HeroNewSemiBold}) format('woff');
+    }
 
-  @font-face {
-      font-family: 'hero new';
-      font-style: italic;
-      font-weight: 500;
-      src: local('Hero New SemiBold Italic'), url(${HeroNewSemiBoldItalic}) format('woff');
-  }
+    @font-face {
+        font-family: 'hero new';
+        font-style: italic;
+        font-weight: 500;
+        src: local('Hero New SemiBold Italic'), url(${HeroNewSemiBoldItalic}) format('woff');
+    }
 
-  @font-face {
-      font-family: 'hero new';
-      font-style: normal;
-      font-weight: 600;
-      src: local('Hero New Bold'), url(${HeroNewBold}) format('woff');
-  }
+    @font-face {
+        font-family: 'hero new';
+        font-style: normal;
+        font-weight: 600;
+        src: local('Hero New Bold'), url(${HeroNewBold}) format('woff');
+    }
 
-  @font-face {
-      font-family: 'hero new';
-      font-style: italic;
-      font-weight: 600;
-      src: local('Hero New Bold Italic'), url(${HeroNewBoldItalic}) format('woff');
-  }
+    @font-face {
+        font-family: 'hero new';
+        font-style: italic;
+        font-weight: 600;
+        src: local('Hero New Bold Italic'), url(${HeroNewBoldItalic}) format('woff');
+    }
 
-  html {
-      font-family: hero new, sans-serif;
-      font-size: 62.5%;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-  }
+    html {
+        font-family: hero new, sans-serif;
+        font-size: 62.5%;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
 
-  body {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
+    body {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }  
 `;
