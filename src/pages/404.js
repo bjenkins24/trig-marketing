46aa5ccddf44587c2730a59f6531b13ccd5ff3e4
Brian Jenkins <bjenkins@yourmusiclessons.com>
2020-01-13 16:58:17 -0800

    custom 404

---
+++ src/pages/404.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Huge } from '@trig-app/core-components/dist/Typography';
+import Layout from '../components/Layout';
+
+const Container = styled.div`
+  margin: 9.6rem auto;
+`;
+
+const FourOhFour = () => {
+  return (
+    <Layout>
+      <Container>
+        <Huge
+          color="bc"
+          css={`
+            text-align: center;
+          `}
+        >
+          Oops! 404 nothing found
+        </Huge>
+      </Container>
+    </Layout>
+  );
+};
+
+export default FourOhFour;
