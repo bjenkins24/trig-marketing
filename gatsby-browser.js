aa0297687b4b9af93b69d4fa4c91cb764863d6bd
Brian Jenkins <bjenkins@yourmusiclessons.com>
2020-02-21 17:56:18 -0800

    Try doing it in useEffect

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { StripeProvider } from 'react-stripe-elements';
 import IsomorphicWrapper from './src/helpers/IsomorphicWrapper';
 import './consoleOverrides';
 
-let apiKey = 'pk_test_fa5gJKRjNx5ybeNGn4T2Ughw';
-if (process.env.NODE_ENV === 'production') {
-  apiKey = 'pk_live_hk5KCpK63rHYijfsdYl7qupz';
-}
-
 // eslint-disable-next-line
 const wrapRootElement = ({ element }) => {
-  return (
-    <IsomorphicWrapper>
-      <StripeProvider stripe={window.Stripe(apiKey)}>{element}</StripeProvider>
-    </IsomorphicWrapper>
-  );
+  return <IsomorphicWrapper>{element}</IsomorphicWrapper>;
 };
 
 export { wrapRootElement };
--- src/helpers/IsomorphicWrapper.js
+++ src/helpers/IsomorphicWrapper.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 import { ToastContainer } from '@trig-app/core-components';
+import { StripeProvider } from 'react-stripe-elements';
 import theme from '@trig-app/themes';
 import '@stripe/stripe-js';
+
+let apiKey = 'pk_test_fa5gJKRjNx5ybeNGn4T2Ughw';
+if (process.env.NODE_ENV === 'production') {
+  apiKey = 'pk_live_hk5KCpK63rHYijfsdYl7qupz';
+}
 
 const isomorphicWrapperTypes = {
   children: PropTypes.node.isRequired,
 };
 
 const IsomorphicWrapper = ({ children }) => {
+  const [stripeInstance, setStripeInstance] = useState(null);
+
+  useEffect(() => {
+    setStripeInstance(window.Stripe(apiKey));
+  }, []);
+
   return (
-    <ThemeProvider theme={theme}>
-      <ToastContainer />
-      {children}
-    </ThemeProvider>
+    <StripeProvider stripe={stripeInstance}>
+      <ThemeProvider theme={theme}>
+        <ToastContainer />
+        {children}
+      </ThemeProvider>
+    </StripeProvider>
   );
 };
 
