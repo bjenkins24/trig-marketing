32f437d85873539d8873d4d4e4297c4b0a239848
Brian Jenkins <bjenkins@yourmusiclessons.com>
2020-02-27 16:37:14 -0800

    Make light and dark favicons

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { Location } from '@reach/router';
-import Icon64x64 from '../icons/favicon-64x64.png';
-import Icon32x32 from '../icons/favicon-32x32.png';
-import Icon16x16 from '../icons/favicon-16x16.png';
+import IconLight64x64 from '../icons/favicon-light-64x64.png';
+import IconLight32x32 from '../icons/favicon-light-32x32.png';
+import IconLight16x16 from '../icons/favicon-light-16x16.png';
+import IconDark64x64 from '../icons/favicon-dark-64x64.png';
+import IconDark32x32 from '../icons/favicon-dark-32x32.png';
+import IconDark16x16 from '../icons/favicon-dark-16x16.png';
 import useSiteMetadata from '../helpers/hooks/useSiteMetadata';
 
 const headPropTypes = {
@@ -26,6 +29,13 @@ const Head = ({ pageTitle, socialImageUrl }) => {
     social,
   } = useSiteMetadata();
 
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const matcher = window.matchMedia('(prefers-color-scheme: dark)');
+    if (matcher.matches) setIsDarkMode(true);
+  }, []);
+
   const pageTitleFull = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
 
   return (
@@ -36,7 +46,6 @@ const Head = ({ pageTitle, socialImageUrl }) => {
         return (
           <Helmet>
             <html lang="en" />
-
             <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
             <meta
               content="width=device-width,initial-scale=1.0,user-scalable=yes"
@@ -46,11 +55,9 @@ const Head = ({ pageTitle, socialImageUrl }) => {
             <meta content={pageTitleFull} property="og:title" />
             <meta content={pageTitleFull} name="twitter:title" />
             <title>{pageTitleFull}</title>
-
             <meta content={siteDescription} name="description" />
             <meta content={siteDescription} property="og:description" />
             <meta content={siteDescription} name="twitter:description" />
-
             <meta content="yes" name="apple-mobile-web-app-capable" />
             <meta
               content="black-translucent"
@@ -58,7 +65,6 @@ const Head = ({ pageTitle, socialImageUrl }) => {
             />
             <meta content={themeColor} name="theme-color" />
             <meta content={siteTitle} name="application-name" />
-
             <meta content="website" property="og:type" />
             <meta content={siteTitle} property="og:site_name" />
             <meta content={social.fbAppId} property="fb:app_id" />
@@ -73,9 +79,54 @@ const Head = ({ pageTitle, socialImageUrl }) => {
               rel="canonical"
               href={canonical}
             />
-            <link rel="icon" type="image/png" href={Icon64x64} sizes="64x64" />
-            <link rel="icon" type="image/png" href={Icon32x32} sizes="32x32" />
-            <link rel="icon" type="image/png" href={Icon16x16} sizes="16x16" />
+            {isDarkMode && (
+              <link
+                rel="icon"
+                type="image/png"
+                href={IconDark64x64}
+                sizes="64x64"
+              />
+            )}
+            {isDarkMode && (
+              <link
+                rel="icon"
+                type="image/png"
+                href={IconDark32x32}
+                sizes="32x32"
+              />
+            )}
+            {isDarkMode && (
+              <link
+                rel="icon"
+                type="image/png"
+                href={IconDark16x16}
+                sizes="16x16"
+              />
+            )}
+            {!isDarkMode && (
+              <link
+                rel="icon"
+                type="image/png"
+                href={IconLight64x64}
+                sizes="64x64"
+              />
+            )}
+            {!isDarkMode && (
+              <link
+                rel="icon"
+                type="image/png"
+                href={IconLight32x32}
+                sizes="32x32"
+              />
+            )}
+            {!isDarkMode && (
+              <link
+                rel="icon"
+                type="image/png"
+                href={IconLight16x16}
+                sizes="16x16"
+              />
+            )}
             {socialImageUrl && (
               <>
                 <meta content={socialImageUrl} property="og:image" />
@@ -89,7 +140,6 @@ const Head = ({ pageTitle, socialImageUrl }) => {
                 <meta content="512" property="og:image:height" />
               </>
             )}
-
             <meta content={themeColor} name="msapplication-TileColor" />
           </Helmet>
         );
